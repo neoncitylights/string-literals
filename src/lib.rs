27780083ad44f3a49f56795a0e0893ddb114408f
Samantha <contact@samanthanguyen.me>
2024-05-15 00:12:05 -0500

    docs: add README usage + crate-level docs (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,50 @@
+/*!
+A very tiny crate with Rust macros to more easily create String types.
+
+When creating string literals in Rust, the given type is `&str`. To create an owned `String` type,
+you either need to:
+
+- pass in the literal to `String::from()`,
+- call `.to_owned()`, or `.to_string()` on the `&str` literal
+
+### Strings
+
+```rust
+use string_literals::s;
+
+let old = "Hello, world!".to_owned();
+let new = s!("Hello, world!");
+```
+
+### Arrays, vectors
+
+```rust
+use string_literals::{string_arr, string_vec};
+
+let old_arr: [String; 3] = ["Banana".to_owned(), "Apple".to_owned(), "Orange".to_owned()];
+let new_arr: [String; 3] = string_arr!["Banana", "Apple", "Orange"];
+
+let old_vec = vec!["Banana".to_owned(), "Apple".to_owned(), "Orange".to_owned()];
+let new_vec = string_vec!["Banana", "Apple", "Orange"];
+```
+
+### Hash maps
+
+```rust
+use std::collections::HashMap;
+use string_literals::string_hashmap;
+
+let mut old: HashMap<String, String> = HashMap::new();
+old.insert("Banana".to_owned(), "Good".to_owned());
+old.insert("Apple".to_owned(), "Okay".to_owned());
+
+let new: HashMap<String, String> = string_literals::string_hashmap! {
+    "Banana" => "Good",
+    "Apple" => "Okay",
+};
+```
+*/
+
 /// Create a [`String`] literal
 ///
 /// This provides a slightly shorter alternative to creating a `String`
@@ -94,7 +141,7 @@ macro_rules! string_vec {
 	};
 }
 
-/// Create a [`HashMap`](std::collections::HashMap) of [`HashMap<String, String>`] with string literals
+/// Create a [`HashMap`](std::collections::HashMap) of `HashMap<String, String>` with string literals
 ///
 /// This macro also allows zero arguments (an empty hash map). In this case
 /// however, it would be shorter in length to call `HashMap::new()`.
